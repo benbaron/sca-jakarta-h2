69fc1b21effe4ae4212f18eff6d36d8528c1ca8d
benbaron <benbaron00@gmail.com>
2026-03-16 18:49:59 -0600

    ignore target dir

--- src/test/java/org/nonprofitbookkeeping/service/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Tests for service-layer parsing and accounting behavior.
- *
- * <p><strong>Dependencies:</strong> JUnit and AssertJ, plus fixtures from
- * {@code org.nonprofitbookkeeping.service}.
- */
-package org.nonprofitbookkeeping.service;
