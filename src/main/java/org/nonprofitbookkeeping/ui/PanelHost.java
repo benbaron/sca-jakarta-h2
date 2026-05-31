535c0b6a35844a358c96f0d392184937614122e4
Ben Baron <benbaron00@gmail.com>
2026-05-31 11:28:45 -0600

    Keep admin panes hosted inside Settings for first admin slice

--- src/main/java/org/nonprofitbookkeeping/ui/PanelHost.java
+++ src/main/java/org/nonprofitbookkeeping/ui/PanelHost.java
@@ -41,10 +41,6 @@ public class PanelHost extends BorderPane
 
         FACTORIES.put(AppPanelId.CHART_OF_ACCOUNTS, ChartOfAccountsPanel::new);
         FACTORIES.put(AppPanelId.FUNDS, FundsPanel::new);
-
-        FACTORIES.put(AppPanelId.COMPANY_ADMIN, CompanyAdminPanel::new);
-        FACTORIES.put(AppPanelId.USER_ADMIN, UserAdminPanel::new);
-
         FACTORIES.put(AppPanelId.SETTINGS, SettingsPanel::new);
         FACTORIES.put(AppPanelId.DIAGNOSTICS, DiagnosticsPanel::new);
         FACTORIES.put(AppPanelId.HELP, HelpPanel::new);
