49b0c3dfcedd90def51a8970a70ea3af9fda2178
Ben Baron <benbaron00@gmail.com>
2026-06-29 09:38:01 -0600

    Document closed period policy type

--- src/main/java/org/nonprofitbookkeeping/model/ClosedPeriodPolicy.java
+++ src/main/java/org/nonprofitbookkeeping/model/ClosedPeriodPolicy.java
@@ -1,5 +1,8 @@
 package org.nonprofitbookkeeping.model;
 
+/**
+ * Defines how the application handles attempts to change a closed period.
+ */
 public enum ClosedPeriodPolicy
 {
     WARN_AND_REOPEN,
