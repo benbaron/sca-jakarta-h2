2250e81cfc26ad9e46e70e4f1ec1b071d3a2b03d
Ben Baron <benbaron00@gmail.com>
2026-07-09 17:32:04 -0600

    Add GridPane compatibility wrapper

---
+++ src/main/java/org/nonprofitbookkeeping/ui/GridPane.java
@@ -0,0 +1,6 @@
+package org.nonprofitbookkeeping.ui;
+
+/** Local alias so TransactionEditorPanel can use GridPane without an explicit import. */
+final class GridPane extends javafx.scene.layout.GridPane
+{
+}
