57e88b00ad5dac918bab2527628f44d0f41c5001
Ben Baron <benbaron00@gmail.com>
2026-07-13 08:40:22 -0600

    Expose company and user administration navigation

--- src/main/java/org/nonprofitbookkeeping/ui/NavigationPane.java
+++ src/main/java/org/nonprofitbookkeeping/ui/NavigationPane.java
@@ -88,6 +88,8 @@ public class NavigationPane extends VBox
         addItem(content, AppPanelId.REPORT_LIBRARY, "Report Library", UiIcons.Glyph.REPORT);
 
         section(content, "ADMINISTRATION");
+        addItem(content, AppPanelId.COMPANY_ADMIN, "Company Admin", UiIcons.Glyph.ACCOUNTS);
+        addItem(content, AppPanelId.USER_ADMIN, "User Admin", UiIcons.Glyph.USERS);
         addItem(content, AppPanelId.CHART_OF_ACCOUNTS, "Chart of Accounts", UiIcons.Glyph.ACCOUNTS);
         addItem(content, AppPanelId.FUNDS, "Funds", UiIcons.Glyph.FUNDS);
         addItem(content, AppPanelId.SETTINGS, "Settings", UiIcons.Glyph.SETTINGS);
