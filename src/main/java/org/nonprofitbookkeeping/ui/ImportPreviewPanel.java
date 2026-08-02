5fae0b1b8dbe65a7071c3581f4cd4ce3a40e0ee0
Ben Baron <benbaron00@gmail.com>
2026-08-02 17:08:04 -0600

    P15-S6-C4 import List for bank preview rows

--- src/main/java/org/nonprofitbookkeeping/ui/ImportPreviewPanel.java
+++ src/main/java/org/nonprofitbookkeeping/ui/ImportPreviewPanel.java
@@ -48,6 +48,7 @@ import org.nonprofitbookkeeping.service.ImportPreviewService;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.function.Function;
