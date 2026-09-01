d6fa6886acaa3f32198ab39c687698b1fc7abd27
Ben Baron <benbaron00@gmail.com>
2026-08-31 20:26:53 -0600

    Fix Journal authorization session fixture

--- src/test/java/org/nonprofitbookkeeping/service/TransactionAuthorizationIntegrationTest.java
+++ src/test/java/org/nonprofitbookkeeping/service/TransactionAuthorizationIntegrationTest.java
@@ -255,7 +255,6 @@ class TransactionAuthorizationIntegrationTest
                 "Operator",
                 companyCode,
                 roles,
-                now,
                 now);
     }
 
